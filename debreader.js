9c34c36a37d170a1476ca8a13e5ac51512703f25
liushuyu <liushuyu011@gmail.com>
2017-02-24 18:15:03 -0700

    parse: parse control file to js object

--- debreader.js
+++ debreader.js
@@ -1,17 +1,39 @@
 'use strict';
 (() => {
-  const addon = require('bindings')('addon');
-  const path = require('path');
-  const fs = require('fs');
-  exports.read = (filename, callback) => {
-    if (typeof callback !== 'function') {
-      throw new TypeError('Callback is not a function');
+const addon = require('bindings')('addon');
+const path = require('path');
+const fs = require('fs');
+
+function parseControl(content, callback) {
+  var control = content.trim().split('\n');
+  var parsed = {};
+  for (let item of control) {
+    let key, value;
+    [key, value] = item.split(': ');
+    parsed[key] = value;
+  }
+  callback(parsed);
+}
+
+exports.read = (filename, callback) => {
+  if (typeof callback !== 'function') {
+    throw new TypeError('Callback is not a function');
+  }
+  fs.access(filename, fs.constants.R_OK, (err) => {
+    if (err) {
+      callback(err, null);
     }
-    fs.access(filename, fs.constants.R_OK, (err) => {
-      if (err) {callback(err, null);}
-      addon.parse(path.resolve(filename), (err, res) => {
+    addon.parse(path.resolve(filename), (err, res) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      parseControl(res.controlFile, (result) => {
+        res.controlFile = result;
         callback(err, res);
       });
     });
-  }
+  });
+}
+
 })();
