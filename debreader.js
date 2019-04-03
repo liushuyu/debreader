60d24d5e6f1981d840c4da261ef983bb65b08ef7
liushuyu <liushuyu011@gmail.com>
2019-04-03 00:31:23 -0600

    deps: update dependencies and format js

--- debreader.js
+++ debreader.js
@@ -1,44 +1,44 @@
 'use strict';
 (() => {
-const addon = require('bindings')('addon');
-const path = require('path');
-const fs = require('fs');
+  const addon = require('bindings')('addon');
+  const path = require('path');
+  const fs = require('fs');
 
-function parseControl(content, callback) {
-  var control = content.trim().split('\n');
-  var parsed = {};
-  for (let item of control) {
-    let key, value;
-    [key, value] = item.split(': ');
-    parsed[key] = value;
-  }
-  callback(parsed);
-}
-
-exports.read = (filename, callback, opts={}) => {
-  if (typeof callback !== 'function') {
-    throw new TypeError('Callback is not a function');
-  }
-  fs.access(filename, fs.constants.R_OK, (err) => {
-    if (err) {
-      callback(err, null);
+  function parseControl(content, callback) {
+    var control = content.trim().split('\n');
+    var parsed = {};
+    for (let item of control) {
+      let key, value;
+      [key, value] = item.split(': ');
+      parsed[key] = value;
     }
-    addon.parse(path.resolve(filename),(err, res) => {
+    callback(parsed);
+  }
+
+  exports.read = (filename, callback, opts = {}) => {
+    if (typeof callback !== 'function') {
+      throw new TypeError('Callback is not a function');
+    }
+    fs.access(filename, fs.constants.R_OK, (err) => {
       if (err) {
         callback(err, null);
-        return;
       }
-      if (!opts.parse) {
-        res.controlFile = res.controlFile.trim();
-        callback(err, res);
-        return;
-      }
-      parseControl(res.controlFile, (result) => {
-        res.controlFile = result;
-        callback(err, res);
-      }, opts.controlOnly);
+      addon.parse(filename, (err, res) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        if (!opts.parse) {
+          res.controlFile = res.controlFile.trim();
+          callback(err, res);
+          return;
+        }
+        parseControl(res.controlFile, (result) => {
+          res.controlFile = result;
+          callback(err, res);
+        }, opts.controlOnly);
+      });
     });
-  });
-}
+  }
 
 })();
