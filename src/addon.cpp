d6200aec117c5c93a0bbb6ca3dbdf5bdf77e0010
liushuyu <liushuyu011@gmail.com>
2019-09-11 20:43:28 -0600

    nan: update and support NodeJS 12

--- src/addon.cpp
+++ src/addon.cpp
@@ -3,12 +3,13 @@
 
 // Exported to JS world
 static void parse(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
   v8::Local<v8::String> opts = info[0].As<v8::String>();
   bool ls = false;
   if (info.Length() > 1) {
-    ls = info[1]->BooleanValue();
+    ls = Nan::To<bool>(info[1]).FromJust();
   }
-  std::string fn(*v8::String::Utf8Value(opts->ToString()));
+  std::string fn(*v8::String::Utf8Value(info.GetIsolate(), opts->ToString(context).ToLocalChecked()));
   Nan::Callback *callback = new Nan::Callback(info[1].As<v8::Function>());
   Nan::AsyncQueueWorker(new DebReader(callback, fn, ls));
   return;
